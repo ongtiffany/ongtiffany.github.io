a07deee6817073847ca7a403728c43fcde66fb57
Tiffany Ong <tiffanyongv@gmail.com>
2026-05-25 03:41:31 +0100

    Add files via upload

--- js/layout.js
+++ js/layout.js
@@ -27,12 +27,14 @@ function loadLayout() {
       <div id="project"></div>
 
       <footer role="contentinfo">
-        <span class="footer-name">Please do not use images or content without permission. <br>Copyright © Tiffany Ong 2026</span>
+       
         <nav class="footer-links">
           <a href="https://www.linkedin.com/in/tiffanyong" target="_blank">LinkedIn</a>
           <a href="https://github.com/ongtiffany" target="_blank">GitHub</a>
           <a href="https://codepen.io/ongtiffany" target="_blank">CodePen</a>
         </nav>
+
+         <div class="footer-name">Copyright © Tiffany Ong 2026 <br> <span>Certain images are sourced from Pexels, Unsplash, past collaborative projects, or original. Please do not use images or content without permission.</span></div>
       </footer>
 
     </div>
